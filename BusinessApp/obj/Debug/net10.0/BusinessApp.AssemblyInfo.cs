2aab6cefd5aa49bbd97c9c19c00134827a7286d8
Lahiru Shamika <159902795+LahiruShamika@users.noreply.github.com>
2025-11-25 14:08:13 +0530

    Create UploadController.cs

--- BusinessApp/obj/Debug/net10.0/BusinessApp.AssemblyInfo.cs
+++ BusinessApp/obj/Debug/net10.0/BusinessApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ceabd6d0c1240d388d6bcd7afac97122fb7da5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf22f1c498fd7f25e59160278990656cd13158f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

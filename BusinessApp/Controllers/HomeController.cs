cf22f1c498fd7f25e59160278990656cd13158f1
Lahiru Shamika <159902795+LahiruShamika@users.noreply.github.com>
2025-11-25 14:06:17 +0530

    Update Home Controller

--- BusinessApp/Controllers/HomeController.cs
+++ BusinessApp/Controllers/HomeController.cs
@@ -2,23 +2,18 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BusinessApp.Models;
 
-namespace BusinessApp.Controllers;
-
-public class HomeController : Controller
+namespace BusinessApp.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
